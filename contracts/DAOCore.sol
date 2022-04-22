7e9cc96f8a4e416ee357961215a00b90f5f40a0d
benjaminsalon <benjaminsalon.business@gmail.com>
2022-04-23 01:51:19 +0200

    End of first day

--- contracts/DAOCore.sol
+++ contracts/DAOCore.sol
@@ -24,7 +24,36 @@ contract DAO {
 
     string public name;
 
+    Vote[] public votes;
+
+    struct Proposal {
+        string name;
+        uint voteNumber;
+    }
+
+    struct Vote {
+        Proposal[] proposals;
+        uint endingTime;
+    }
+
     constructor(string memory _name) {
         name = _name;
     }
+
+    modifier isVotePossible(uint _voteId){
+        require(block.timestamp < votes[_voteId].endingTime, "Vote is no more possible");
+        _;
+    }
+
+    function proposeVote(uint _endingTime, Proposal[] memory _startingProposals) external {
+        Vote memory newVote = Vote({endingTime: _endingTime, proposals:_startingProposals});
+        votes.push(newVote);
+    }
+
+    function addProposalToVote(uint _voteId, Proposal memory _proposal) external isVotePossible(_voteId) {
+        votes[_voteId].proposals.push(_proposal);
+    }
+    
+
+
 }
